523457df65ed215abf270f054ccd8ce05a3b0848
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-10-14 17:06:37 +0300

    add day 5

--- controlStructures.js
+++ controlStructures.js
@@ -5,24 +5,72 @@ let num3 = 3;
 console.log(num1 == num2);
 console.log(num1 % 2 == 0);
 
-let ar = [num1, num2, num3];
-ar = ar.sort(function (a, b) {
-  return b - a;
-});
-console.log(ar);
+if (num1 > num2 && num1 > num3) {
+  if (num2 > num3) {
+    console.log(num1, num2, num3);
+  } else {
+    console.log(num1, num3, num2);
+  }
+} else if (num2 > num1 && num2 > num3) {
+  if (num1 > num3) {
+    console.log(num2, num1, num3);
+  } else {
+    console.log(num2, num3, num1);
+  }
+} else if (num3 > num1 && num3 > num2) {
+  if (num1 > num2) {
+    console.log(num3, num1, num2);
+  } else {
+    console.log(num3, num2, num1);
+  }
+}
+
+let amount = "ABC";
+if (amount < 0) {
+  console.log("Please enter a positive number");
+} else if (isNaN(amount)) {
+  console.log("Please enter a number");
+} else {
+  console.log(amount);
+}
+
+let username = "Abdullah";
+let password = "123456789";
+
+if (username.length > 6 && password.length >= 8) {
+  console.log("login successful");
+} else {
+  console.log("login failed.");
+}
+
+let a = 0;
+while (a <=20 ) {
+  console.log(a);
+  a++;
+}
 
 for (let i = 0; i < 21; i++) {
   console.log(i);
 }
-let a = 0;
-while (a > -21) {
-  console.log(a);
-  a--;
+
+for (i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
 }
-//*************************************
+
+let startRange =5;
+let endRange =8;
+for (let i=startRange; i<=endRange; i++){
+  console.log(i);
+}
+
+
+let star=''
 for (let i = 0; i < 6; i++) {
   for (let j = 0; j < i; j++) {
-    console.log("*");
-  }
-  console.log("/n");
+    star= star + '*'
+    }
+  console.log(star);
+  star =''
 }
